43074ddd0ac18ba213bfd355e021d46acd7dc6d3
Alexis Carlson <whatisthehumanspirit@gmail.com>
2016-10-17 11:55:14 -0700

    Prepare to use bundled client JS.

--- www/assets/javascripts/app.js
+++ www/assets/javascripts/app.js
@@ -1,3 +1,11 @@
+var EWD = require('ewd-client').EWD;
+var io = require('socket.io-client')
+var jQuery = require('jquery');
+window.$ = window.jQuery = jQuery;
+require('bootstrap');
+var toastr = require('toastr');
+var login = require('ewd-vista-login/client/vista-login');
+
 var VISTA = {};
 VISTA.application = {};
 VISTA.application.name = 'vista.login';
