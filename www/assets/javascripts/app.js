3fb01a9aa66b02ca5fb3eadc7faae6596e5cd942
Alexis Carlson <whatisthehumanspirit@gmail.com>
2016-10-18 13:39:50 -0700

    Remove Bed Board code. Abandon VISTA namespace.

--- www/assets/javascripts/app.js
+++ www/assets/javascripts/app.js
@@ -6,9 +6,6 @@ require('bootstrap');
 var toastr = require('toastr');
 var login  = require('ewd-vista-login/client/vista-login');
 
-var VISTA     = {};
-VISTA.appName = 'ewd-vista';
-
 /*
   This section starts everything. If you are following
   the code, start here.
@@ -28,7 +25,7 @@ $(document).ready(function() {
       location.reload()
     });
     
-    VISTA.preLogin1();
+    login.preLogin1(EWD);
   });
 
   /* This is good for testing, but I don't want it normally.
@@ -39,61 +36,5 @@ $(document).ready(function() {
   });
   */
 
-  EWD.start(VISTA.appName, $, io);
+  EWD.start('ewd-vista', $, io);
 });
-
-/* ---------------- */
-/* THIS IS THE FIRST NON-LOGIN RELATED FUNCTION */
-VISTA.showApplication = function () {
-    VISTA.initNavBar();
-    // Uncomment to use Bed Board module
-    // VISTA.showWards();
-}
-
-// Uncomment to use Bed Board module
-//
-// Show Wards and Beds. Called from show application
-// VISTA.showWards = function() {
-//   EWD.send({type: 'wards'}, function(responseObj) {
-//     var wards = responseObj.message.wards;
-//     console.log(wards);
-//
-//     wards.forEach(function(ward, index, array) {
-//       var html = '<div class="main col-md-4"><h2 class="sub-header">';
-//       html     = html + ward.name + '</h2><div class="table-responsive">';
-//       html     = html + '<table class="table table-striped"><thead><tr><td>Bed</td>';
-//       html     = html + '<td>Patient</td><td>Gender</td><td>Admission Date</td>';
-//       html     = html + '</tr></thead><tbody>';
-//
-//       var beds = ward.beds;
-//       beds.forEach(function(bed, index, array) {
-//         html = html + '<tr><td>' + bed.name;
-//         if (bed.oos) {
-//           html = html + ' <span class="glyphicon glyphicon-exclamation-sign"';
-//           html = html + ' aria-hidden="true" title="' + bed.oos + '"></span>';
-//         }
-//         html = html + '</td><td>';
-//         if (bed.patient.name) {
-//           html = html + bed.patient.name;
-//         }
-//         html = html + '</td><td>';
-//         if (bed.patient.sex) {
-//           html = html + bed.patient.sex;
-//         }
-//         html = html + '</td><td>';
-//         if (bed.patient.admissionDate) {
-//           html = html + bed.patient.admissionDate.replace(/@.*/, '');
-//         }
-//         html = html + '</td></tr>';
-//       });
-//
-//       html = html + '</tbody></table></div></div>';
-//
-//       $('#wards').append(html);
-//     });
-//
-//     $('#wards glyphicon-exclamation-sign').hover()
-//
-//     $('#wards').show();
-//   });
-// }
