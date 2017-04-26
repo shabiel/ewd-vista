0971616d87087222d432c21988d8676989602436
Sam Habiel <sam.habiel@gmail.com>
2017-04-26 14:17:51 -0700

    Do entry point until db.procedure is available

--- routines/ewdVistAUtils.m
+++ routines/ewdVistAUtils.m
@@ -1,13 +1,6 @@
-ewdVistAUtils ; VEN/ARC - VistA utilities ; 4/17/17 7:52pm
- ;
- ; 2016-12-27 ARC: Blah blah
- ;
- quit
- ;
+ewdVistAUtils ; VEN/ARC - VistA utilities ; 4/26/17 1:55pm
  ;
 GetVar(var) ;
- ;ARC;private;test;clean;silent;non-SAC
- ;
  ; If you want to see what's going on, comment these in, and kill ^SAM in Prog Mode (GT.M Only).
  ; N % S %=$I(^SAM)
  ; ZSHOW "V":^SAM(%)
@@ -16,19 +9,16 @@ GetVar(var) ;
  ;
  ;
 SetVar(var,val) ;
- ;ARC;private;test;clean;silent;non-SAC
- ;
  set @var=val
- ;
  quit @var
  ;
  ;
 KillVar(var) ;
- ;ARC;private;test;clean;silent;non-SAC
- ;
  kill @var
- ;
  quit 1
  ;
+D(entryref) ; (only for Cache as we use db.procedure on GT.M
+ do @entryref
+ quit ""
  ;
 EOR ; End of routine ewdVistAUtils
