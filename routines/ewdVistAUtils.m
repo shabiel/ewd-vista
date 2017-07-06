0aa7df559e582abb4649c7ee74199bde41c5d4b4
Sam Habiel <sam.habiel@gmail.com>
2017-07-06 14:32:47 -0400

    functions moved to ewdSymbolTable

--- routines/ewdVistAUtils.m
+++ routines/ewdVistAUtils.m
@@ -1,24 +1,5 @@
-ewdVistAUtils ; VEN/ARC - VistA utilities ; 4/26/17 1:55pm
- ;
-GetVar(var) ;
- ; If you want to see what's going on, comment these in, and kill ^SAM in Prog Mode (GT.M Only).
- ; N % S %=$I(^SAM)
- ; ZSHOW "V":^SAM(%)
- ;
- quit $$GETV^XWBPRS(var)
- ;
- ;
-SetVar(var,val) ;
- set @var=val
- quit @var
- ;
- ;
-KillVar(var) ;
- kill @var
- quit 1
+ewdVistAUtils ; VEN/ARC - VistA utilities ;2017-07-06  2:26 PM
  ;
 D(entryref) ; (only for Cache as we use db.procedure on GT.M
  do @entryref
  quit ""
- ;
-EOR ; End of routine ewdVistAUtils
