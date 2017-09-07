cc0844201fab22e4fdf6cf08f158fff9538d41b4
Sam Habiel <sam.habiel@gmail.com>
2017-09-07 12:43:38 -0400

    agencyIsVA and agencyIsIHS implementation

--- routines/ewdVistAUtils.m
+++ routines/ewdVistAUtils.m
@@ -1,5 +1,5 @@
-ewdVistAUtils ; VEN/ARC - VistA utilities ;2017-07-06  2:26 PM
+ewdVistAUtils ; VEN/ARC - VistA utilities ;2017-09-07  12:21 PM
  ;
-D(entryref) ; (only for Cache as we use db.procedure on GT.M
+D(entryref) ; for Cache and for reference calls as well which can't be done via NodeM/cache.node
  do @entryref
  quit ""
